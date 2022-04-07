415b3d45e2a40674cc9ba4d4e4cbacda709a5414
Michael Brandt <msbrandt00@gmail.com>
2022-04-06 20:32:36 -0600

    add production URL to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3001', 'ancient-spire-22400.herokuapp.com']
+    origins "http://localhost:3001"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: true
+  allow do
+    origins "https://cryptic-scrubland-50039.herokuapp.com/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
